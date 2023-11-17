45df5f4e09f056b4a54396131de65556829f7418
ivanillo2001 <ivanjimenezquintero@outlook.com>
2023-11-17 09:46:18 +0100

    Primera subida

---
+++ js/app.js
@@ -0,0 +1,34 @@
+"use strict";
+//declaramos el array de las posiciones
+let aPosiciones = [];
+document.addEventListener("DOMContentLoaded", () => {
+  crearTablero(); //nada más empezar se crea el tablero
+});
+
+function crearTablero() {
+  let contadorPosiciones = -1;
+  const tablero = document.querySelector(".tablero");
+  for (let i = 0; i < 10; i++) {
+    const fila = document.createElement("tr");
+    for (let index = 0; index < 10; index++) {
+      contadorPosiciones++;
+      const celda = document.createElement("td");
+      celda.setAttribute("id", contadorPosiciones);
+      if (celda.getAttribute("id") === "0") {
+        //si la celda es la 0 se llama a la funcion agregarImagenInicio
+        agregarImagenInicio(celda,"door_opened");
+      } else if (celda.getAttribute("id")==="99") {
+        //si la celda tiene id 99 llama a funcion para agregarle la clase
+        agregarImagenInicio(celda,"exploradora");
+      }
+      fila.appendChild(celda);
+    }
+    tablero.appendChild(fila);
+  }
+}
+
+function agregarImagenInicio(carta,clase) {
+    const imagen = document.createElement("img");
+    imagen.classList.add(clase);
+    carta.appendChild(imagen);
+}
